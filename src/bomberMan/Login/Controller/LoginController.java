bee6c359b5f08d2fb26e20b0ed4cf9cdf858bab9
Tong Yi <tong.yi@mail.mcgill.ca>
2014-11-08 17:40:38 -0500

    adding controller prototype

--- src/bomberMan/Login/Controller/LoginController.java
+++ src/bomberMan/Login/Controller/LoginController.java
@@ -1,5 +1,64 @@
 package bomberMan.Login.Controller;
 
-public class LoginController {
+public class LoginController extends Controller {
 
+	private String username;
+	private String password;
+
+	public void checkNextView(int buttonPressed) {
+		if (buttonPressed == 1) {
+			// checks if it logs in properly
+			if (checkLogin(username, password) == true
+					&& (username != null && password != null)) {
+
+				// UserDatabase.setCurrentView("MainMenu");
+				System.out.println("Set Current View: MainMenu");
+				System.out.println("Set Current User: " + username);
+				// UserDatabase.setCurrentUser(LoginView.getUsername());
+			}
+
+			else {
+
+				System.out.println("Set Current View: Login");
+				loginError();
+
+				// UserDatabase.setNextView("Login");
+			}
+
+		}
+
+		else if (buttonPressed == 2) {
+
+			System.out.println("Set Current View: SignUp");
+			// UserDatabase.setCurrentView("SignUp");
+		}
+
+		else if (buttonPressed == 3) {
+			Controller.exitGame();
+			// ExitController.exitGame();
+		}
+	}
+
+	public boolean checkLogin(String username, String password) {
+		if (username == "bob" && password == "abc123") {
+			return true;
+		}
+
+		else {
+			return false;
+		}
+	}
+
+	public void loginError() {
+		System.out.println("Login Error");
+		// call some method in the view to display error message.
+	}
+
+	public void setUserName(String userName) {
+		username = userName;
+	}
+
+	public void setPassword(String passWord) {
+		password = passWord;
+	}
 }
--- src/bomberMan/Login/Controller/SignUpController.java
+++ src/bomberMan/Login/Controller/SignUpController.java
@@ -1,5 +1,60 @@
 package bomberMan.Login.Controller;
 
-public class SignUpController {
+public class SignUpController extends Controller {
 
+	private String username;
+	private String password;
+	private String fullName;
+	
+	public void checkNextView(int buttonPressed) {
+		// Sign up button
+		if (buttonPressed == 1) {
+			if (checkUsernameNotTaken(username) == true) {
+				// need to call method to set new user name and password in
+				// model
+				System.out.println("You have successfully signed up!");
+				System.out.println("Set current view: Login");
+			} 
+			
+			else {
+				System.out.println("Set current view: SignUp");
+				usernameErrorDisplay();
+			}
+
+		}
+		// login button
+		else if (buttonPressed == 2) {
+			System.out.println("Set current view: Login");
+		}
+		// Exit button
+		else if (buttonPressed == 3) {
+			Controller.exitGame();
+		}
+	}
+
+	public boolean checkUsernameNotTaken(String username) {
+		if (username == "Bob") {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public void usernameErrorDisplay() {
+		// call some method in view to display message.
+		System.out.println("Username already taken");
+
+	}
+	
+	public void setUserName(String userName){
+		username = userName;
+	}
+	
+	public void setPassWord(String passWord){
+		password = passWord;
+	}
+	
+	public void setFullName(String fullname){
+		fullName = fullname;
+	}
 }
