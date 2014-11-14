967bdce637267543478566d50f6d7d5b8a349257
Tong Yi <tong.yi@mail.mcgill.ca>
2014-11-13 21:08:37 -0500

    made small corrections for the signup controller

--- src/bomberMan/Login/Controller/LoginController.java
+++ src/bomberMan/Login/Controller/LoginController.java
@@ -4,16 +4,13 @@ import bomberMan.Login.Model.UserDatabase;
 
 public class LoginController{
 	private UserDatabase DB;
-	public LoginController(UserDatabase database)
-	{
+	public LoginController(UserDatabase database){
 		this.DB=database;
 	}
 	//login button being pressed, returns 0 or 1 depending if it is successful
 	public int login(String username, String password)
 	{
 		if(DB.login(username, password) == true){
-			System.out.println("Goes into MainMenu");
-			System.out.println("set current user: " + username);
 			return 0;
 			}
 			
