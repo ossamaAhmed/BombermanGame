f0c9e0d2a81b6fa413f065cfb13c25da64c40d6e
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2014-11-25 15:40:07 -0500

    Implementing exit door

---
+++ src/bomberMan/gamePlay/Model/ExitDoor1.java
@@ -0,0 +1,12 @@
+package bomberMan.gamePlay.Model;
+
+public class ExitDoor1 extends GameObject {
+
+	public ExitDoor1(int xPos,int yPos,String location, String objectType) {
+		super(xPos, yPos, location, objectType);
+	}
+}
+	
+	
+	
+	
--- src/bomberMan/gamePlay/Model/GameBoard.java
+++ src/bomberMan/gamePlay/Model/GameBoard.java
@@ -246,7 +246,7 @@ public class GameBoard implements java.io.Serializable {
 				
 				 board[i][j].insert(new Wall(j*CONSTANTS.TILE_SIDE_SIZE,i*CONSTANTS.TILE_SIDE_SIZE,WallType.BRICK));
 				 if(exitAlreadyPlaced == false && counter == exitNumberChosen){
-					 board[i][j].insert(new GameObject(j*CONSTANTS.TILE_SIDE_SIZE,i*CONSTANTS.TILE_SIDE_SIZE,CONSTANTS.EXIT_IMAGE, "ExitDoor"));
+					 board[i][j].insert(new ExitDoor1(j*CONSTANTS.TILE_SIDE_SIZE,i*CONSTANTS.TILE_SIDE_SIZE,CONSTANTS.EXIT_IMAGE, "ExitDoor"));
 				     exitAlreadyPlaced = true;
 				     System.out.println("NUMBER CHOSEN FOR EXIT i j " + i + ", "+j);
 				 }
