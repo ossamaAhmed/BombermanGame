4d52d936e93466e13d95ab5a3d1d288f73d770e1
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2014-11-25 18:32:48 -0500

    Adding some corrections

--- src/bomberMan/gamePlay/Model/ExitDoor1.java
+++ src/bomberMan/gamePlay/Model/ExitDoor1.java
@@ -1,6 +1,6 @@
 package bomberMan.gamePlay.Model;
 
-public class ExitDoor1 extends GameObject {
+public class ExitDoor1 extends GameObject implements java.io.Serializable {
 
 	public ExitDoor1(int xPos,int yPos,String location, String objectType) {
 		super(xPos, yPos, location, objectType);
