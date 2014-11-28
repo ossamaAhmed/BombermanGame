03ef01e8e45ebd5ff5c7ae1aa876e9cd5bca99c2
AndresFelipeRG <andres.rincon@mail.mcgill.ca>
2014-11-27 23:19:01 -0500

    AGAIN

--- Tests/gameModelTests/BomberManTest.java
+++ Tests/gameModelTests/BomberManTest.java
@@ -2,12 +2,119 @@ package gameModelTests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.junit.*;
 
 import bomberMan.gamePlay.Model.*;
 public class BomberManTest {
-
+     private  BomberMan bmb1;
+    @Before
+    public void setUp(){
+		
+	}
+	 @Test
+	 public void testBomberManConstructor()
+		{
+			
+		}
+	@Test
+	public void testInfoToString() {
+			
+		}
+	@Test
+	public void testAddBomb(){}
+	@Test
+	public void testGetICell(){
+			
+		}
+	@Test
+	public void testGetJCell(){
+		
+	}
+	@Test
+    public void testGetICellBottomBomberman(){
+			
+	}
+	@Test
+	public void  testGetJCellRightMostBomberman(){
+		
+	}
+	@Test
+	public void testRemoveOldestBomb(){
+			
+		}
+	@Test
+	public void testRemoveBomb(){
+			
+		}
+	@Test	
+	public void testGetOldestBomb(){}
+			
+	@Test
+	public void testDie()
+		{
+			
+			
+		}	
+		
+	@Test	
+	public void testGetHasDetonator(){}
+	@Test
+	public void testAddPowerUp(){}
+	@Test
+	public void testSetHasDetonator(){}
+	@Test
+	public void testGetBombRange(){}
+	@Test
+	public void testSetBombRange(){}
+	@Test
+	public void testSetBombRange1(){}
+	@Test
+	public void testGetNumBombsToDrop(){}
+	@Test
+	public void testSetNumBombsToDrop(){}
+	@Test
+	public void testSetNumBombsToDrop1(){}
+	@Test
+	public void testSetBrickPass(){}
+	@Test
+	public void testGetBrickPass(){}
+	@Test
+	public void testSetBombPass(){}
+	@Test
+	public void testgetBombPass(){}
+	@Test
+	public void testSetFlamePass(){}
+	@Test
+	public void testGetFlamePass(){}
+	@Test
+	public void testGetQteOfBombsDropped(){}
+	@Test
+	public void testSetQteOfBombsDropped(){}
+	@Test
+	public void testGetInvisibilibityPowerUp(){}
+	@Test
+	public void testSetInvisibilityPowerUp(){
+			}
+	@Test
+	public void testGetCreationInvisibilityPowerUp(){}
+	@Test
+	public void tesSetCreationInvisibilityPowerUp(){
+			
+		}
+	@Test
+	public void testAddTimeCreationInvisibilitPowerUp(){}
+	@Test
+	public void testGetEliminationInvisibilityPowerUp(){}
+	@Test
+	public void testGetPowerUpsKeptAfterDeath(){
+			
+			
+		}
+		
+	
 	
 
 }
